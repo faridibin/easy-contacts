51f96c87fb471504535dc284915ff3fdff74c432
Farid Adam <faridibin@gmail.com>
2019-09-12 00:10:33 -0400

    Controllers

---
+++ src/Http/Controllers/EasyFormsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Faridibin\EasyForms\Http\Controllers;
+
+use Faridibin\EasyForms\Http\Requests\ContactRequest;
+use Faridibin\EasyForms\Mail\ContactMailable;
+use Faridibin\EasyForms\Models\Contact;
+use Illuminate\Support\Facades\Mail;
+
+class EasyFormsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('easy-forms::contact');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ContactRequest $request)
+    {
+        $contacted = Contact::create($request->all());
+
+        Mail::to('faridibin@gmail.com')->send(new ContactMailable($request->all())); //custom mail
+
+        return redirect()->route('contact.index')->with('sent', 'Message Sent!'); //custom message
+
+    }
+}
