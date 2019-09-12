f6d21e891b7681c6f6b8e4bad1ad22cfe5f3df44
Farid Adam <faridibin@gmail.com>
2019-09-12 00:13:18 -0400

    Mailable

---
+++ src/Mail/ContactMailable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Faridibin\EasyForms\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ContactMailable extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $mail;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($request)
+    {
+        $this->mail = $request;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('contact.email')->with(['mail' => $this->mail]);
+    }
+}
