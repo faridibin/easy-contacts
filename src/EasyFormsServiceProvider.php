53d9847da20b12881a46925b5188cabdb44f16a9
Farid Adam <faridibin@gmail.com>
2019-09-12 00:10:54 -0400

    Package Service Provider

---
+++ src/EasyFormsServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Faridibin\EasyForms;
+
+use Illuminate\Support\ServiceProvider;
+
+class EasyFormsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'easy-forms');
+
+        // $this->publishes([
+        //     __DIR__.'/path/to/config/courier.php' => config_path('courier.php'),
+        // ], 'config');
+
+        // $this->publishes([
+        //     __DIR__.'/../database/migrations/' => database_path('migrations')
+        // ], 'migrations');
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // $this->mergeConfigFrom(
+        //     __DIR__.'/path/to/config/courier.php', 'courier'
+        // );
+    }
+}
