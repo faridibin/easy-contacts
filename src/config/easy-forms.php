ce1870004aa3b5a215b7c8b2be7df9a63d1f283c
Farid Adam <faridibin@gmail.com>
2019-09-13 17:34:51 -0400

    Configuration for Easy Forms Package

---
+++ src/config/easy-forms.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mail To
+    |--------------------------------------------------------------------------
+    |
+    | This value is the email that would be sent the message from the contact form.
+    | This value is used when the package needs to include an email address in a
+    | notification or any other location as required by the package.
+    |
+    */
+
+    'mail_to' => 'email@domain.com',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Form Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of the form. This value is used when the
+    | package needs to show the form in the application.
+    |
+    */
+
+    'form_name' => 'Contact Form',
+
+
+];
